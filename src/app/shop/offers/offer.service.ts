416b31d3a33664331c9ed32f9a596bc736fcbf48
acalvom <andreac.3108@gmail.com>
2021-02-23 21:45:38 +0100

    list significant fields from offers in "offers" menu

--- src/app/shop/offers/offer.service.ts
+++ src/app/shop/offers/offer.service.ts
@@ -23,24 +23,9 @@ export class OfferService {
 
   search(offerSearch: OfferSearch): Observable<Offer[]> {
     return of([
-      {
-        reference: '123abc', description: 'offer1', creationDate: new Date('2019-01-16'),
-        expiryDate: new Date('2019-03-16'), discount: 10,
-        articles: [{barcode: '123', description: '...', retailPrice: 10, providerCompany: 'pro1'},
-          {barcode: '234', description: '...', retailPrice: 20, providerCompany: 'pro2'},
-          {barcode: '345', description: '...', retailPrice: 30, providerCompany: 'pro3'}]
-      },
-      {
-        reference: '234bcd', description: 'offer2', creationDate: new Date('2019-07-16'),
-        expiryDate: new Date('2019-08-08'), discount: 20,
-        articles: [{barcode: '123', description: '...', retailPrice: 10, providerCompany: 'pro1'},
-          {barcode: '456', description: '...', retailPrice: 40, providerCompany: 'pro4'}]
-      },
-      {
-        reference: '345cde', description: 'offer3', creationDate: new Date('2020-05-20'),
-        expiryDate: new Date('2021-08-13'), discount: 30,
-        articles: []
-      }
+      {reference: '123abc', description: 'offer1', expiryDate: new Date('2019-03-16'), discount: 10},
+      {reference: '234bcd', description: 'offer2', expiryDate: new Date('2019-08-08'), discount: 20},
+      {reference: '345cde', description: 'offer3', expiryDate: new Date('2021-08-13'), discount: 30}
     ]);
 
     /*return this.httpService
