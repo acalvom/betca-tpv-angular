701b922318f8ada4b05e4156da04407c49e650fd
acalvom <andreac.3108@gmail.com>
2021-02-23 19:09:30 +0100

    list offers in "offers" menu

--- src/app/shop/offers/offer.service.ts
+++ src/app/shop/offers/offer.service.ts
@@ -1,9 +1,11 @@
 import {Injectable} from '@angular/core';
 import {HttpService} from '@core/http.service';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {EndPoints} from '@shared/end-points';
 import {OfferSearch} from './offer-search.model';
 import {Offer} from './offer.model';
+import {Article} from '../shared/services/models/article.model';
+
 
 @Injectable({
   providedIn: 'root',
@@ -15,9 +17,34 @@ export class OfferService {
   constructor(private httpService: HttpService) {
   }
 
+  /*  articles: Article[] = [{barcode: '123', description: '...', retailPrice: 10, providerCompany: 'pro1'},
+      {barcode: '234', description: '...', retailPrice: 20, providerCompany: 'pro2'},
+      {barcode: '345', description: '...', retailPrice: 30, providerCompany: 'pro3'}];*/
+
   search(offerSearch: OfferSearch): Observable<Offer[]> {
-    return this.httpService
+    return of([
+      {
+        reference: '123abc', description: 'offer1', creationDate: new Date('2019-01-16'),
+        expiryDate: new Date('2019-03-16'), discount: 10,
+        articles: [{barcode: '123', description: '...', retailPrice: 10, providerCompany: 'pro1'},
+          {barcode: '234', description: '...', retailPrice: 20, providerCompany: 'pro2'},
+          {barcode: '345', description: '...', retailPrice: 30, providerCompany: 'pro3'}]
+      },
+      {
+        reference: '234bcd', description: 'offer2', creationDate: new Date('2019-07-16'),
+        expiryDate: new Date('2019-08-08'), discount: 20,
+        articles: [{barcode: '123', description: '...', retailPrice: 10, providerCompany: 'pro1'},
+          {barcode: '456', description: '...', retailPrice: 40, providerCompany: 'pro4'}]
+      },
+      {
+        reference: '345cde', description: 'offer3', creationDate: new Date('2020-05-20'),
+        expiryDate: new Date('2021-08-13'), discount: 30,
+        articles: []
+      }
+    ]);
+
+    /*return this.httpService
       .paramsFrom(offerSearch)
-      .get(EndPoints.OFFERS + OfferService.SEARCH);
+      .get(EndPoints.OFFERS + OfferService.SEARCH);*/
   }
 }
