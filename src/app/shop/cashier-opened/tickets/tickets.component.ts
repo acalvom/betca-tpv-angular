44b633c575d5c55f4afb2d5ba07137707468cb8d
aleandrorsanchez <aleeexr7@gmail.com>
2021-02-23 18:03:04 +0100

    tickets.component.html sin servicio #1

--- src/app/shop/cashier-opened/tickets/tickets.component.ts
+++ src/app/shop/cashier-opened/tickets/tickets.component.ts
@@ -1,16 +1,30 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {of} from 'rxjs';
 
 @Component({
   selector: 'app-ticket',
   templateUrl: './tickets.component.html',
   styleUrls: ['./tickets.component.css']
 })
-export class TicketsComponent implements OnInit {
+export class TicketsComponent{
+
+  title = 'Tickets Management';
+  key: string;
+  tickets = of([]);
 
   constructor() {
   }
 
-  ngOnInit(): void {
+  search(): void {
+
   }
 
+  resetSearch(): void {
+    this.key = '';
+    this.tickets = null;
+  }
+
+  update($event: any): void {
+
+  }
 }
