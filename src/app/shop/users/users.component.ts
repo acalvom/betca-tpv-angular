dd060b10f95efecca52953d09adf94985c433ed2
Hector Muñoz Mérida <hector.mumartin@gmail.com>
2021-02-26 17:57:06 +0100

    Edit user role using mocks #15

--- src/app/shop/users/users.component.ts
+++ src/app/shop/users/users.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import {of} from 'rxjs';
 import {UserSearch} from './user-search-model';
 import {UserService} from './user.service';
+import {Article} from '../shared/services/models/article.model';
+import {ArticleCreationUpdatingDialogComponent} from '../articles/article-creation-updating-dialog.component';
+import {User} from '@core/user.model';
+import {MatDialog} from '@angular/material/dialog';
+import {UserCreationUpdatingDialogComponent} from './user-creation-updating-dialog.component';
+import {Role} from '@core/role.model';
 
 @Component({
   selector: 'app-users',
@@ -14,7 +20,7 @@ export class UsersComponent {
   title = 'Users management';
   users = of([]);
 
-  constructor(private userService: UserService) {
+  constructor(private dialog: MatDialog, private userService: UserService) {
     this.resetSearch();
   }
 
@@ -28,6 +34,14 @@ export class UsersComponent {
 
   unfinished(): void {
     this.users = this.userService.searchUnfinished();
+  }
+
+  update(user: User): void {
+    of(
+      {mobile: 1234324, name: 'Hector', role: Role.CUSTOMER, token: ' '},
+    ).subscribe(fullUser => this.dialog.open(UserCreationUpdatingDialogComponent, {data: fullUser}));
+    /*this.userService.read(user.mobile)
+      .subscribe(fullUser => this.dialog.open(UserCreationUpdatingDialogComponent, {data: fullUser}));*/
   }
 
 }
