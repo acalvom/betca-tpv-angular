1c0d35526ad2abe13d26dd5a6e4f642d90318876
aleandrorsanchez <aleeexr7@gmail.com>
2021-02-27 11:35:41 +0100

    decreaseAmount() and update shopping list #1

--- src/app/shop/cashier-opened/tickets/ticket-editing-dialog.component.ts
+++ src/app/shop/cashier-opened/tickets/ticket-editing-dialog.component.ts
@@ -1,28 +1,46 @@
-import {Component, Inject} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {Shopping} from '../../shared/services/models/shopping.model';
-import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {MAT_DIALOG_DATA, MatDialog} from '@angular/material/dialog';
+import {NumberDialogComponent} from '@shared/dialogs/number-dialog.component';
+import {ShoppingState} from '../shopping-cart/shopping-state.model';
 
 @Component({
   selector: 'app-ticket-editing-dialog',
   templateUrl: './ticket-editing-dialog.component.html',
   styleUrls: ['./ticket-editing-dialog.component.css']
 })
-export class TicketEditingDialogComponent{
+export class TicketEditingDialogComponent implements OnInit{
 
   indexShoppingList: 0;
   displayedColumns = ['id', 'description', 'retailPrice', 'amount', 'discount', 'total', 'actions'];
   shoppingList: Shopping[];
+  totalShoppingList = 0 ;
 
-  constructor(@Inject(MAT_DIALOG_DATA) data: Shopping[]) {
+  constructor(@Inject(MAT_DIALOG_DATA) data: Shopping[], private dialog: MatDialog) {
     this.shoppingList = data ? data : undefined;
   }
 
-  update(): void {
-
+  ngOnInit(): void {
+    this.synchronizeShoppingCart();
   }
 
-  decreaseAmount(item: any): void {
+  synchronizeShoppingCart(): void {
+    this.shoppingList = [...this.shoppingList];
+    let total = 0;
+    for (const shopping of this.shoppingList) {
+      total = total + shopping.total;
+    }
+    this.totalShoppingList = Math.round(total * 100) / 100;
+  }
 
+  decreaseAmount(shopping: Shopping): void {
+    shopping.amount--;
+    if (shopping.amount === 0) {
+      shopping.amount--;
+      shopping.state = ShoppingState.COMMITTED;
+    }
+    shopping.updateTotal();
+    this.synchronizeShoppingCart();
   }
 
   changeCommitted(item: any): void {
@@ -30,6 +48,10 @@ export class TicketEditingDialogComponent{
   }
 
   checkboxState(state: any): void {
+
+  }
+
+  update(): void {
 
   }
 }
