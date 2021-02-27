3e44a5a7888e8c94a2c3104af0bc71591465f900
acalvom <andreac.3108@gmail.com>
2021-02-27 11:25:25 +0100

    apply offers to the shopping cart and update total #2

--- src/app/shop/cashier-opened/shopping-cart/shopping-cart.component.ts
+++ src/app/shop/cashier-opened/shopping-cart/shopping-cart.component.ts
@@ -171,9 +171,14 @@ export class ShoppingCartComponent implements OnInit {
         this.shoppingCart.forEach(element => {
           const search = newOffer.articles.find(art => art.barcode === element.barcode);
           if (search !== undefined) {
-            console.log(element.barcode + ' matches with ' + search.barcode);
+            element.discount = newOffer.discount;
+            // console.log('article  ' + element.barcode + ' is in the offer ' + newOffer.reference +
+            //   ' and the discount is: ' + newOffer.discount);
+            element.updateTotal();
+            this.synchronizeShoppingCart();
           } else {
-            console.log('article  ' + element.barcode + ' is not in the offer');
+            element.discount = 0;
+            // console.log('article  ' + element.barcode + ' is not in the offer');
           }
         });
         // console.log('reference: ' + newOffer.reference + ' - discount: ' + newOffer.discount + ' - article: ' + art.barcode);
