bfb5ceb083612c302877c48d827672e528ff77a4
Jaime Pérez <jaime.perez@alumnos.upm.es>
2021-02-28 13:43:17 +0100

    Created manage-data-protection-act.component #9

---
+++ src/app/shop/shared/components/manage-data-protection-act.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-data-protection-act',
+  templateUrl: './manage-data-protection-act.component.html',
+  styleUrls: ['./manage-data-protection-act/manage-data-protection-act.component.css']
+})
+export class ManageDataProtectionActComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
