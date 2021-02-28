10266834b519326ec05da81a42ea50935b405967
Luis Conde <brotherowsky@gmail.com>
2021-02-28 15:50:51 +0100

    #16

--- src/app/home/ticket-tracking/ticket-tracking.service.ts
+++ src/app/home/ticket-tracking/ticket-tracking.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Shopping } from '../../shop/shared/services/models/shopping.model';
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,10 @@ import { Injectable } from '@angular/core';
 export class TicketTrackingService {
 
   constructor() { }
+
+  read(): Observable<Shopping[]> {
+    const shopping1: Shopping = new Shopping('11111', 'description1', 6);
+    const shopping2: Shopping = new Shopping('11112', 'description2', 90);
+    return of([shopping1, shopping2]);
+  }
 }
