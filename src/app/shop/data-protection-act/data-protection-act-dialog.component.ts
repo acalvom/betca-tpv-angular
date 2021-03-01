0f0ed5a5777674b23191c529549f69a826819cf0
Jaime Pérez <jaime.perez@alumnos.upm.es>
2021-03-01 20:22:21 +0100

    add data protection act dialog to shop menu

---
+++ src/app/shop/data-protection-act/data-protection-act-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-protection-act-dialog',
+  templateUrl: './data-protection-act-dialog.component.html',
+  styleUrls: ['./data-protection-act-dialog.component.css']
+})
+export class DataProtectionActDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
