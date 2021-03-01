2b2a297bb9f3ea30df3465e2f4eefc3a4b983923
David Amaya <d.acruz@alumnos.upm.es>
2021-03-01 21:24:55 +0100

    Add app-crud stock-alarms issue #8 Add component dialog creating-updating (skeleton)

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -40,6 +40,7 @@ import {ListComponent} from './shared/components/list.component';
 import {NewArticleFamilyDialogComponent} from './articles-family/dialogs/new-article-family-dialog/new-article-family-dialog.component';
 import {EditArticleFamilyDialogComponent} from './articles-family/dialogs/edit-article-family-dialog/edit-article-family-dialog.component';
 import { ManageDataProtectionActComponent } from './shared/components/manage-data-protection-act.component';
+import { StockAlarmsCreationUpdatingDialogComponent } from './stock-alarms/stock-alarms-creation-updating-dialog.component';
 
 
 @NgModule({
@@ -73,6 +74,7 @@ import { ManageDataProtectionActComponent } from './shared/components/manage-dat
     UserCreationUpdatingDialogComponent,
     TicketEditingDialogComponent,
     ManageDataProtectionActComponent,
+    StockAlarmsCreationUpdatingDialogComponent,
   ],
   entryComponents: [
     ArticleCreationUpdatingDialogComponent,
