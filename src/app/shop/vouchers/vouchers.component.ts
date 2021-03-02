f69682d88425eec4fc9f4824c0f910830d9baed9
AlexCabezas2018 <alexcabezas0705@gmail.com>
2021-03-02 19:29:17 +0100

    #32 Add crud component to template Add basic mocked vouchers to service

--- src/app/shop/vouchers/vouchers.component.ts
+++ src/app/shop/vouchers/vouchers.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {VouchersService} from './vouchers.service';
+import {of} from 'rxjs';
+import {Voucher} from '../shared/services/models/voucher.model';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-vouchers',
   templateUrl: './vouchers.component.html',
   styleUrls: ['./vouchers.component.css']
 })
-export class VouchersComponent implements OnInit {
+export class VouchersComponent {
 
-  constructor() { }
+  title = 'Vouchers management';
+  vouchers = of([]);
 
-  ngOnInit(): void {
+  constructor(private dialog: MatDialog, private vouchersService: VouchersService) {
+    this.findAll();
   }
 
+  findAll(): void {
+    this.vouchers = this.vouchersService.findAll();
+  }
+
+  create(): void {
+
+  }
+
+  read(voucher: Voucher): void {
+
+  }
 }
--- src/app/shop/vouchers/vouchers.service.ts
+++ src/app/shop/vouchers/vouchers.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Observable, of} from 'rxjs';
+import {Voucher} from '../shared/services/models/voucher.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VouchersService {
 
-  constructor() { }
+  vouchers: Voucher[];
+
+  constructor() {
+    this.vouchers = [
+      {reference: 'reference1', value: 10, creationDate: new Date(), dateOfUse: new Date()},
+      {reference: 'reference2', value: 20, creationDate: new Date(), dateOfUse: new Date()},
+      {reference: 'reference3', value: 30, creationDate: new Date(), dateOfUse: new Date()},
+      {reference: 'reference4', value: 40, creationDate: new Date(), dateOfUse: new Date()},
+    ];
+  }
+
+  findAll(): Observable<Voucher[]> {
+    return of(this.vouchers);
+  }
 }
