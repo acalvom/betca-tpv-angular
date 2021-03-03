3ce139a5b125c40c17f184bc21a9e91189767fae
ulises <ulises.mateo.ortega@alumnos.upm.es>
2021-03-03 10:23:25 +0100

    Edited news and popular components #11

--- src/app/home/adviser/popular/popular.component.ts
+++ src/app/home/adviser/popular/popular.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Article} from '../../shared/article.model';
 
 @Component({
   selector: 'app-popular',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popular.component.css']
 })
 export class PopularComponent implements OnInit {
+  article1: Article = {barcode: '121212', description: 'Popular articles', retailPrice: 40};
+  article2: Article = {barcode: '777777', description: 'Popular article more', retailPrice: 50};
+  articlesPopular = [this.article1, this.article2, this.article1, this.article2, this.article1];
 
   constructor() { }
 
