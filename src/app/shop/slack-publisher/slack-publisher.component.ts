6f6a6f3c534238bbdb7a8648515bd7ff980f5a16
pablocc <pablocc1996@gmail.com>
2021-03-03 20:32:07 +0100

    Slack dialog and basic template #22

---
+++ src/app/shop/slack-publisher/slack-publisher.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slack-publisher',
+  templateUrl: './slack-publisher.component.html',
+  styleUrls: ['./slack-publisher.component.css']
+})
+export class SlackPublisherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
