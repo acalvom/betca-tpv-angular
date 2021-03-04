608a69fb75760c06c8004cded360d3e7a86629ff
Kazlunn <manuel.martin.hornillos@alumnos.upm.es>
2021-03-04 18:27:10 +0100

    add issue mocks

--- src/app/shop/issues/issue.service.ts
+++ src/app/shop/issues/issue.service.ts
@@ -1,19 +1,41 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpService} from '@core/http.service';
 import {IssueSearch} from './issue-search.model';
 import {Observable, of} from 'rxjs';
 import {Issue} from './issue.model';
+import {IssueState} from './issue-state.enum';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IssueService {
 
+  private issueMocks: Issue[] = [
+    {
+      title: 'Found a bug',
+      body: 'I\'m having a problem with this.',
+      labels: 'bug',
+      state: IssueState.OPEN,
+      assignees: 'octocat',
+      milestone: 'v1.0',
+      created_at: '2011-04-10T20:09:31Z',
+    },
+    {
+      title: 'Enhancement',
+      body: 'This could be improved.',
+      labels: 'enhancement',
+      state: IssueState.OPEN,
+      assignees: 'kazlunn',
+      milestone: 'v1.1',
+      created_at: '2021-03-04T18:09:31Z',
+    },
+  ];
+
   constructor(private httpService: HttpService) {
   }
 
   search(issueSearch: IssueSearch): Observable<Issue[]> {
-    return of([]); // TODO
+    return of(this.issueMocks); // TODO
   }
 
   create(issue: Issue): Observable<Issue> {
