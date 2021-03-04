371bc9ecb8c0f599289ed5b00feb28b3e9aef586
AlexCabezas2018 <alexcabezas0705@gmail.com>
2021-03-04 21:25:36 +0100

    #32 Add filtering of vouchers

--- src/app/shop/vouchers/vouchers.component.ts
+++ src/app/shop/vouchers/vouchers.component.ts
@@ -5,6 +5,7 @@ import {Voucher} from '../shared/services/models/voucher.model';
 import {MatDialog} from '@angular/material/dialog';
 import {ReadDetailDialogComponent} from '@shared/dialogs/read-detail.dialog.component';
 import {VoucherCreationUpdatingDialogComponent} from './voucher-creation-updating-dialog.component';
+import {VoucherSearch} from './voucher.search';
 
 @Component({
   selector: 'app-vouchers',
@@ -15,9 +16,15 @@ export class VouchersComponent {
 
   title = 'Vouchers management';
   vouchers = of([]);
+  voucherSearch: VoucherSearch;
 
   constructor(private dialog: MatDialog, private vouchersService: VouchersService) {
+    this.resetSearch();
     this.findAll();
+  }
+
+  resetSearch(): void {
+    this.voucherSearch = {};
   }
 
   findAll(): void {
@@ -37,5 +44,10 @@ export class VouchersComponent {
         object: this.vouchersService.read(voucher.reference)
       }
     });
+  }
+
+  findVouchersBetweenDates(): void {
+    this.vouchers = this.vouchersService
+      .findVouchersBetweenDates(new Date(this.voucherSearch.dateFrom), new Date(this.voucherSearch.dateTo));
   }
 }
--- src/app/shop/vouchers/vouchers.service.ts
+++ src/app/shop/vouchers/vouchers.service.ts
@@ -10,6 +10,15 @@ export class VouchersService {
 
   vouchers: Voucher[];
 
+  constructor() {
+    this.vouchers = [
+      {reference: 'reference1', value: 10, creationDate: new Date('2021-01-20'), dateOfUse: new Date()},
+      {reference: 'reference2', value: 20, creationDate: new Date('2021-02-28'), dateOfUse: new Date()},
+      {reference: 'reference3', value: 30, creationDate: new Date(), dateOfUse: new Date()},
+      {reference: 'reference4', value: 40, creationDate: new Date('2019-12-31'), dateOfUse: new Date()},
+    ];
+  }
+
   create(voucher: VoucherCreation): Observable<Voucher> {
     const createdVoucher: Voucher = {
       reference: 'created',
@@ -22,20 +31,17 @@ export class VouchersService {
     return of(createdVoucher);
   }
 
-  constructor() {
-    this.vouchers = [
-      {reference: 'reference1', value: 10, creationDate: new Date(), dateOfUse: new Date()},
-      {reference: 'reference2', value: 20, creationDate: new Date(), dateOfUse: new Date()},
-      {reference: 'reference3', value: 30, creationDate: new Date(), dateOfUse: new Date()},
-      {reference: 'reference4', value: 40, creationDate: new Date(), dateOfUse: new Date()},
-    ];
-  }
-
   findAll(): Observable<Voucher[]> {
     return of(this.vouchers);
   }
 
   read(reference: string): Observable<Voucher> {
     return of(this.vouchers.filter(v => v.reference === reference)[0]);
+  }
+
+  findVouchersBetweenDates(dateFrom: Date, dateTo: Date): Observable<Voucher[]> {
+    return of(this.vouchers
+      .filter(voucher => voucher.creationDate.getTime() >= dateFrom.getTime()
+        && voucher.creationDate.getTime() <= dateTo.getTime() && voucher.dateOfUse !== undefined));
   }
 }
