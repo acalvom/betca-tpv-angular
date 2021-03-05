0fe13df02d085657f6292b7c4848e5d51f4741ea
Mikel Sanabria <mikel.sanabria@gmail.com>
2021-03-05 02:19:18 +0100

    implement service read, create, save stock audits #21

--- src/app/shop/stock-audit/stock-audit.service.ts
+++ src/app/shop/stock-audit/stock-audit.service.ts
@@ -6,41 +6,141 @@ import {ArticleLoss} from '../shared/services/models/article-loss.model';
 import {ArticleNotAuditLine} from '../shared/services/models/article-not-audit-line.model';
 import {ArticleAudit} from '../shared/services/models/article-audit.model';
 import {ArticleLossLine} from '../shared/services/models/article-loss-line.model';
+import {StockAudit} from '../shared/services/models/stock-audit.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StockAuditService {
+  private stockAudits: StockAudit[] = ([
+    {
+      idAudit: '1',
+      creationDate: new Date(),
+      closeDate: new Date(),
+      barcodesWithoutAudit: ['100000', '200000'],
+      lossValue: 300,
+      losses: [{barcode: '300000', amount: 3}]
+    },
+    {
+      idAudit: '2',
+      creationDate: new Date(),
+      closeDate: null,
+      barcodesWithoutAudit: ['500000'],
+      lossValue: 40,
+      losses: [{barcode: '600000', amount: 1}]
+    }
+  ]);
+
+  private auditArticles: ArticleAudit [] = ([
+    {
+      // tslint:disable-next-line:max-line-length
+      idAudit: '2', // ID provisional (Simula BBDD) Cuando se llame a la API sólo traerá los de la auditoría que se pasa como parámetro => Método readAuditArticles()
+      barcode: '22222222222',
+      description: 'descripcion1',
+      retailPrice: 33,
+      stock: 2,
+      realStock: 2
+    },
+    {
+      idAudit: '2',
+      barcode: '22222255555',
+      description: 'descripcion2',
+      retailPrice: 36,
+      stock: 20,
+      realStock: 15
+    },
+    {
+      idAudit: '3',
+      barcode: '8904598349435',
+      description: 'descripcion3',
+      retailPrice: 5,
+      stock: 10,
+      realStock: null
+    },
+    {
+      idAudit: '3',
+      barcode: '890459836666',
+      description: 'descripcion4',
+      retailPrice: 50,
+      stock: 15,
+      realStock: null
+    }
+  ]);
 
   constructor(private httpService: HttpService) { }
 
-  readAll(): Observable<ArticleAudit[]> {
+  readSingleOpenedAudit(): Observable<StockAudit> {
     /*return this.httpService
-      .get(EndPoints.AUDIT);*/
-    // empty input field in amount if exist
+      .get(EndPoints.AUDITS + '/opened');*/
+    return of(this.stockAudits.find(stockAudit => stockAudit.closeDate == null));
+  }
 
-    const articleAudit: ArticleAudit [] = ([
+  createAudit(stockAudit: StockAudit): Observable<StockAudit> {
+    /*return this.httpService
+      .post(EndPoints.AUDITS, stockAudit);*/
+    const stockAuditCreated: StockAudit = (
       {
-        barcode: '8904598349435',
-        description: 'descripcion1',
-        retailPrice: 5,
-        stock: 10,
-        realStock: 7
-      },
-      {
-        barcode: '890459836666',
-        description: 'descripcion2',
-        retailPrice: 50,
-        stock: 15,
-        realStock: null
-      }
-    ]);
-    return of(articleAudit);
+        idAudit: '3',
+        creationDate: new Date(),
+        closeDate: null,
+        barcodesWithoutAudit: [],
+        lossValue: null,
+        losses: []
+      });
+    return of(stockAuditCreated);
+  }
+
+  readAuditArticles(idAudit: string): Observable<ArticleAudit[]> {
+    /*return this.httpService
+      .get(EndPoints.AUDITS + '/' + idAudit + '/articles');*/
+    return of(this.auditArticles.filter(auditArticle => auditArticle.idAudit === idAudit));
+  }
+
+  saveAudit(idAudit: string, auditArticles: ArticleAudit[]): Observable<ArticleAudit[]> {
+    /*return this.httpService
+      .patch(EndPoints.AUDITS + '/' + idAudit + '/articles', auditArticles);*/
+    return of([{
+      // tslint:disable-next-line:max-line-length
+      idAudit: '2', // ID provisional (Simula BBDD) Cuando se llame a la API sólo traerá los de la auditoría que se pasa como parámetro => Método readAuditArticles()
+      barcode: '22222222222',
+      description: 'descripcion1',
+      retailPrice: 33,
+      stock: 2,
+      realStock: 1 // Simula que hemos puesto 1 en el primer elemento
+  }, {
+      idAudit: '2',
+      barcode: '22222255555',
+      description: 'descripcion2',
+      retailPrice: 36,
+      stock: 20,
+      realStock: 15 // Queda igual
+    }]);
+  }
+
+  closeAudit(idAudit: string, auditArticles: ArticleAudit[]): Observable<ArticleAudit[]> {
+    /*return this.httpService
+      .patch(EndPoints.AUDITS + '/' + idAudit, auditArticles);*/
+    return of([{
+      // tslint:disable-next-line:max-line-length
+      idAudit: '2', // ID provisional (Simula BBDD) Cuando se llame a la API sólo traerá los de la auditoría que se pasa como parámetro => Método readAuditArticles()
+      barcode: '22222222222',
+      description: 'descripcion1',
+      retailPrice: 33,
+      stock: 2,
+      realStock: 1 // Simula que hemos puesto 1 en el primer elemento
+    }, {
+      idAudit: '2',
+      barcode: '22222255555',
+      description: 'descripcion2',
+      retailPrice: 36,
+      stock: 20,
+      realStock: 15 // Queda igual
+    }]);
   }
 
   getNotAudit(idAudit: string): Observable<ArticleNotAuditLine[]> {
     /*return this.httpService
-      .get(EndPoints.AUDITS + '/not-audit/' + idAudit);*/
+      .get(EndPoints.AUDITS + '/' + idAudit + '/not-audit');*/
     const articleNotAuditLine: ArticleNotAuditLine [] = ([
       {
         barcode: '8904598349435',
@@ -52,14 +152,9 @@ export class StockAuditService {
     return of(articleNotAuditLine);
   }
 
-  closeAudit(idAudit: string, barcodesWithoutAudit: string[], articlesLoss: ArticleLoss[]): void {
-    /*return this.httpService
-      .patch(EndPoints.AUDITS + '/' + idAudit, {closeDate: new Date(), barcodesWithoutAudit: barcodesWithoutAudit, losses: articlesLoss});*/
-  }
-
   getLosses(idAudit: string): Observable<ArticleLossLine[]>{
     /*return this.httpService
-      .get(EndPoints.AUDITS + '/not-audit/' + idAudit);*/
+      .get(EndPoints.AUDITS + '/' + idAudit + '/losses');*/
     const articleLossLine: ArticleLossLine [] = ([
       {
         barcode: '890459836666',
