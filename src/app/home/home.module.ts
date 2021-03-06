8806a165519cb55af79475fcfd04f8f5780fee1c
Patricia403 <736026544@qq.com>
2021-03-06 17:49:19 +0100

    fix

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -15,11 +15,12 @@ import { ScoreBarComponent } from './reviews/score-bar/score-bar.component';
 import { OffersComponent } from './offers/offers.component';
 import { ProductComponent } from './ticket-tracking/product/product.component';
 import { IvyCarouselModule } from 'angular-responsive-carousel';
-import {ShoppingBasketComponent} from './shopping-basket/shopping-basket.component';
+import { ShoppingBasketComponent } from './shopping-basket/shopping-basket.component';
 import { Top5Component } from './top5/top5.component';
 import { StockManagementComponent } from './stock-management/stock-management.component';
 import { NewsComponent } from './adviser/news/news.component';
 import { PopularComponent } from './adviser/popular/popular.component';
+import { OnlineOrderComponent } from './online-order/online-order.component';
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { PopularComponent } from './adviser/popular/popular.component';
     StockManagementComponent,
     NewsComponent,
     PopularComponent,
+    OnlineOrderComponent,
   ],
   entryComponents: [
     ComplaintCreationDialogComponent,
--- src/app/home/shopping-basket/shopping-basket.component.ts
+++ src/app/home/shopping-basket/shopping-basket.component.ts
@@ -1,14 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+
+import {LoginDialogComponent} from '@shared/dialogs/login-dialog.component';
+import {MatDialog} from '@angular/material/dialog';
+import {AuthService} from '@core/auth.service';
+import {CheckOutDialogComponent} from '../../shop/cashier-opened/shopping-cart/check-out-dialog.component';
+import {Article} from "../shared/article.model";
+
 
 @Component({
+  selector: 'app-shopping-basket',
   templateUrl: './shopping-basket.component.html',
   styleUrls: ['./shopping-basket.component.css']
 })
 export class ShoppingBasketComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['position', 'barcode', 'description', 'retailPrice'];
+  article1: Article = {barcode: '000001', description: 'dagahajq', retailPrice: 23.67};
+  article2: Article = {barcode: '000002', description: 'hfgdsagh', retailPrice: 10.89};
+  article3: Article = {barcode: '000003', description: 'asdfghjg', retailPrice: 34.78};
+  article4: Article = {barcode: '000004', description: 'jghfdssa', retailPrice: 23.09};
+  article5: Article = {barcode: '000005', description: 'zxcngvhj', retailPrice: 5.20};
+  shoppingBasket = [this.article1, this.article2, this.article3, this.article4, this.article5];
+  dataSource = this.shoppingBasket;
+  username = undefined;
+  totalShoppingBasket = 0 ;
+
+  constructor(private dialog: MatDialog, private authService: AuthService) {
+
+  }
 
   ngOnInit(): void {
+
+  }
+
+  login(): void {
+    this.dialog.open(LoginDialogComponent)
+      .afterClosed()
+      .subscribe(() => this.username = this.authService.getName());
+  }
+
+  isAuthenticated(): boolean {
+    return this.authService.isAuthenticated();
+  }
+
+  pay() {
+    this.dialog.open(CheckOutDialogComponent, {data: this.shoppingBasket}).afterClosed().subscribe(
+      result => {
+        if (result) {
+          this.ngOnInit();
+        }
+      }
+    );
   }
 
 }
