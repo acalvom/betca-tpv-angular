80b58c5d252c563f9cca1c3c8b58121ac9aa283a
acalvom <andreac.3108@gmail.com>
2021-03-10 14:23:16 +0100

    change the dialog view to an Offer Details page view #69

--- src/app/home/offers/offers.component.ts
+++ src/app/home/offers/offers.component.ts
@@ -1,17 +1,16 @@
 import {Component, OnInit} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
-import {ReadDetailDialogComponent} from '@shared/dialogs/read-detail.dialog.component';
 import {OfferService} from './offer.service';
 import {ActivatedRoute, Router} from '@angular/router';
-
+import {Offer} from './offer.model';
 
 @Component({
   templateUrl: './offers.component.html',
 })
 export class OffersComponent implements OnInit {
   private reference: string;
-
   parameterValue: string;
+  offer: Offer;
 
   constructor(private dialog: MatDialog, private offerService: OfferService,
               private router: Router, private activatedRoute: ActivatedRoute) {
@@ -19,18 +18,9 @@ export class OffersComponent implements OnInit {
 
   ngOnInit(): void {
     this.reference = this.activatedRoute.snapshot.paramMap.get('reference');
-    console.log('Reference: ' + this.reference);
-    this.dialog.open(ReadDetailDialogComponent, {
-      data: {
-        title: 'Offer Details',
-        object: this.offerService.read(this.reference)
-      }
-    });
-
     this.parameterValue = this.reference;
-    this.activatedRoute.params.subscribe(parameter => {
-      this.parameterValue = parameter.key;
-      console.log(parameter.reference);
-    });
+    this.activatedRoute.params.subscribe(parameter => this.parameterValue = parameter.key);
+    this.offerService.read(this.reference).subscribe(offer => this.offer = offer);
+
   }
 }
