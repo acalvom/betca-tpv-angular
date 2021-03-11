a1bb26677d5a04d6d0a864febab710273a8cbf76
aamarinho <drealvarez97@gmail.com>
2021-03-11 20:33:50 +0100

    Modified model and view (new attribute barcode) #51

--- src/app/shop/shared/services/shared.articles-family.service.ts
+++ src/app/shop/shared/services/shared.articles-family.service.ts
@@ -4,7 +4,6 @@ import {Observable, of} from 'rxjs';
 import {ArticleFamilyViewModel} from '../../cashier-opened/shopping-cart/article-family-view/article-family-view.model';
 import {EndPoints} from '@shared/end-points';
 import {HttpService} from '@core/http.service';
-import {TreetypeModel} from './models/treetype.model';
 
 @Injectable({
   providedIn: 'root'
@@ -31,53 +30,6 @@ export class SharedArticlesFamilyService {
       }
     ]
   }
-  ];
-
-  ARTICLES_FAMILY_DATA: ArticleFamilyModel[] = [
-    {
-      reference: '1',
-      description: 'Zarzuela',
-      treeType: 'composite'
-    },
-    {
-      reference: '1',
-      description: 'Varios',
-      treeType: 'composite',
-    }
-  ];
-
-  CHILDRENS_OF_ZZ: ArticleFamilyViewModel[] = [
-    {
-      reference: '8400000000031',
-      description: 'descrip-a3',
-      type: TreetypeModel.ARTICLE,
-      price: 10.12,
-    },
-    {
-      reference: '1',
-      description: 'Zz Falda',
-      type: TreetypeModel.SIZES
-    },
-    {
-      reference: '2',
-      description: 'Zz Falda',
-      type: TreetypeModel.SIZES
-    }
-  ];
-
-  ARTICLES: ArticleFamilyViewModel [] = [
-    {
-      reference: '8400000000017',
-      description: 'Zarzuela - falda T2',
-      type: TreetypeModel.ARTICLE,
-      price: 20,
-    },
-    {
-      reference: '8400000000024',
-      description: 'Zarzuela - falda T4',
-      type: TreetypeModel.ARTICLE,
-      price: 27.8,
-    }
   ];
 
   constructor(private httpService: HttpService) {
